53a81f3d5bda028c0b22044388dc4d788b0d1fdf
Ritesh268 <riteshyaduvanshi777@gmail.com>
2024-07-30 15:14:35 +0530

    remove unused code

--- lib/utils/api_path.dart
+++ lib/utils/api_path.dart
@@ -1,5 +1,4 @@
 abstract class ApiConstants {
   static String contentjson = 'application/json';
-  static String authorizationToken = '';
   static String baseUrl = 'https://fakestoreapi.com/products';
 }
